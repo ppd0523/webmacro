653fd83d7503216ebf0e71a7c824893d22274d52
Park <ppd0523@bbrick.io>
2021-11-29 19:15:22 +0900

    button event prototype
    * window destroy
    * async button

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tkinter as tk, sys
+import tkinter as tk
+import threading
+from functools import partial
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchWindowException
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,27 +11,51 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
-URL = 'https://www.google.co.kr'
+service = Service(ChromeDriverManager().install())
+driver = None
+init_url = 'https://www.google.co.kr'
 
-# service = Service(ChromeDriverManager().install())
-# driver = webdriver.Chrome(service=service)
-# driver.get(url=URL)
+def open_browser():
+    def task():
+        global driver
+        try:
+            windows = driver.window_handles
+            if windows:
+                return
+        except AttributeError as e:
+            if driver:
+                return
 
-import functools
-def print_func(text):
-    f = functools.partial(print, text)
-    return f
+        driver = webdriver.Chrome(service=service)
+        init_url = 'https://www.google.co.kr'
+        driver.get(url=init_url)
+
+    threading.Thread(target=task).start()
+
+
+def on_destroy(_root):
+    global driver
+    if driver:
+        try:
+            driver.close()
+        except NoSuchWindowException:
+            pass
+    _root.destroy()
+
 
 root = tk.Tk()
-root.title("Web Macro")
+root.title("Web Controller")
 root.geometry("480x320+100+100")
 root.resizable(True, True)
 
-button1 = tk.Button(root, text='sample', width=15, height=15, command=print_func('hello'))
-button2 = tk.Button(root, text='sample', width=15, height=15, command=print_func('world'))
-button1.pack()
-button2.pack()
+frame = tk.Frame(root, relief="solid", bd=1)
+frame.pack(side="left", fill="both", expand=False)
 
+button1 = tk.Button(frame, text='Open\nChrome', width=5, height=5, command=open_browser)
+button1.pack()
+
+on_destroy = partial(on_destroy, root)
+root.protocol("WM_DELETE_WINDOW", on_destroy)
 root.mainloop()
 
 # try:
